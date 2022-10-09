5dd801d7ca3298614c75b0d780e2baff9ad3b240
Alexandru Florea <alexflorea16000@gmail.com>
2022-10-09 13:06:09 +0300

    Add objects by clicking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BoxGeometry, BufferGeometry, DirectionalLight, Material, Mesh, MeshPhongMaterial, PerspectiveCamera, PointLight, Raycaster, Scene, Vector2, WebGLRenderer } from 'three';
+import { AmbientLight, AxesHelper, BoxGeometry, BufferGeometry, DirectionalLight, Material, Mesh, MeshPhongMaterial, PerspectiveCamera, PlaneGeometry, PointLight, Quaternion, Raycaster, Scene, Vector2, Vector3, WebGLRenderer } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 class SceneObject {
@@ -16,6 +16,19 @@ class SceneObject {
   getMesh(): Mesh {
     return this.mesh;
   }
+
+
+
+  rotate(amount: Vector3) {
+    // this.geometry.rotateX(- Math.PI / 2);
+    this.geometry.rotateX(amount.x);
+    this.geometry.rotateY(amount.y);
+    this.geometry.rotateZ(amount.z);
+  }
+
+  move(position: Vector3) {
+    this.geometry.translate(position.x, position.y, position.z);
+  }
 }
 
 @Component({
@@ -28,19 +41,27 @@ export class AppComponent implements OnInit {
 
   private scene = new Scene();
   private camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  private renderer = new WebGLRenderer();
+  private renderer = new WebGLRenderer({ antialias: true });
 
   private raycaster = new Raycaster();
   private pointer = new Vector2();
 
+  private intersectCubeShown = false;
+  private intersectCube = new SceneObject(new BoxGeometry(0.5, 0.3, 0.1), new MeshPhongMaterial({ color: 0x00ff00 }))
+
   ngOnInit() {
     this.setupScene();
 
-    const cube = new SceneObject(new BoxGeometry(2, 1, 3), new MeshPhongMaterial({ color: 0xefefef }));
+    const cube = new SceneObject(new BoxGeometry(2, 1, 3), new MeshPhongMaterial({ color: 0xcdcdcd }));
     this.scene.add(cube.getMesh());
 
+    const plane = new SceneObject(new PlaneGeometry(10000, 10000), new MeshPhongMaterial({ color: 0xffffff}));
+    plane.rotate(new Vector3(- Math.PI / 2, 0, 0));
+    plane.move(new Vector3(0, -0.5, 0));
+    this.scene.add(plane.getMesh())
+
     // TODO: on click add a new item to the 'warehouse'
-    const intersectCube = new SceneObject(new BoxGeometry(0.1, 0.1, 0.1), new MeshPhongMaterial({ color: 0x00ff00 }));
+    const initialFront = new Vector3(0, 0, 1); // of the intersect cube
 
     const animate = () => {
       requestAnimationFrame(animate);
@@ -48,14 +69,19 @@ export class AppComponent implements OnInit {
       this.raycaster.setFromCamera(this.pointer, this.camera);
       const intersects = this.raycaster.intersectObjects([cube.getMesh()]);
       if (intersects[0]) {
+        const intersectNormal = intersects[0].face?.normal;
         const intersectPosition = intersects[0].point;
-        intersectCube.getMesh().position.set(intersectPosition.x, intersectPosition.y, intersectPosition.z);
+        this.intersectCube.getMesh().position.set(intersectPosition.x, intersectPosition.y, intersectPosition.z);
+        // rotate the cube so its Z axis is aligned with the normal of the face
+        this.intersectCube.getMesh().setRotationFromQuaternion(new Quaternion().setFromUnitVectors(initialFront, intersectNormal!));
         
-        if (!this.scene.children.includes(intersectCube.getMesh())) {
-          this.scene.add(intersectCube.getMesh());
+        if (!this.scene.children.includes(this.intersectCube.getMesh())) {
+          this.scene.add(this.intersectCube.getMesh());
+          this.intersectCubeShown = true;
         }
       } else {
-        this.scene.remove(intersectCube.getMesh());
+        this.scene.remove(this.intersectCube.getMesh());
+        this.intersectCubeShown = false;
       }
 
       this.renderer.render(this.scene, this.camera);
@@ -68,6 +94,15 @@ export class AppComponent implements OnInit {
     this.pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
     this.pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
   }
+  
+  private onClick = (event: any) => {
+    if (this.intersectCubeShown) {
+      const newObject = this.intersectCube.getMesh().clone();
+      newObject.material = new MeshPhongMaterial({ color: 0x555555 });
+
+      this.scene.add(newObject);
+    }
+  }
 
   private setupScene() {
     this.camera.position.set(2.5, 2.5, 5)
@@ -75,16 +110,26 @@ export class AppComponent implements OnInit {
     const directionalLight = new DirectionalLight(0xffffff, 0.75);
     directionalLight.position.set(-5, -2.5, -3);
     const pointLight = new PointLight(0xffffff, 1);
-    pointLight.position.set(5, 5, 5);
+    pointLight.position.set(5, 10, 5);
+    const ambientLight = new AmbientLight(0xffffff, 0.1);
 
     this.scene.add(pointLight);
     this.scene.add(directionalLight);
+    this.scene.add(ambientLight);
 
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
 
-    const controls = new OrbitControls(this.camera, this.renderer.domElement)
+    const controls = new OrbitControls(this.camera, this.renderer.domElement) // controls.update() must be called every time the position of the camera is modified externally
 
     window.addEventListener('pointermove', this.onPointerMove);
+    window.addEventListener('click', this.onClick);
+
+    // this.setupDebug();
+  }
+
+  private setupDebug() {
+    const axesHelper = new AxesHelper(5);
+    this.scene.add(axesHelper);
   }
 }
